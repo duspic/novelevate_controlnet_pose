ff8ee1aaeb1e06eedbb3478c1003d05b877aaa3a
duspic <duspic77@gmail.com>
2023-05-10 12:35:15 +0200

    fix Image for pasting

--- predict.py
+++ predict.py
@@ -63,6 +63,7 @@ class Predictor(BasePredictor):
         if not os.path.exists("tmp"):
             os.mkdir("tmp")
         
-        outputs = [output.paste(input_image, mask=input_image) for output in outputs]
+        noback_img = Image.open(image)
+        outputs = [output.paste(noback_img, mask=noback_img) for output in outputs]
         outputs = [output.save(f"tmp/output_{i}.png") for i, output in enumerate(outputs)]
         return [Path(f"./tmp/output_{i}.png") for i in range(len(outputs))]
