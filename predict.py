3bf8403d96c7116b3526ab10c73ccc0f99995c20
duspic <duspic77@gmail.com>
2023-05-19 18:10:43 +0200

    random fix

--- predict.py
+++ predict.py
@@ -13,7 +13,7 @@ import numpy as np
 
 class Predictor(BasePredictor):
     def setup(self):
-       self.model = Model(base_model_id='ducnapa/children_stories_inpaint')
+       self.model = Model(base_model_id='ducnapa/children_stories_inpainting')
 
     def predict(
         self,
