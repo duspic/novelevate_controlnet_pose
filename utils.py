812bbbab24d6396a51ff11146a4fef70a447b773
duspic <duspic77@gmail.com>
2023-05-26 13:09:50 +0200

    fix utils

--- utils.py
+++ utils.py
@@ -1,18 +1,18 @@
 from PIL import Image
 
 def make_sheet(img: Image.Image, color: int=255) -> Image.Image:
-  res = Image.new('RGB', (1024,512),color=color)
+  res = Image.new('RGBA', (1024,512),color=(color,color,color,color))
   w,h = img.size
   h_offset = int((512-h)/2)
   w_offset = int((256-w)/2)
   for i in range(4):
-    res.paste(img, box=(i*256 + w_offset,h_offset))
+    res.paste(img, mask=img, box=(i*256 + w_offset,h_offset))
 
   return res.convert('RGB')
 
 def make_mask() -> Image.Image:
-  res = Image.new('RGB', (1024,512), color=0)
-  white = Image.new('RGB', (256,512), color=255)
+  res = Image.new('RGB', (1024,512), color=(0,0,0))
+  white = Image.new('RGB', (256,512), color=(255,255,255))
 
   res.paste(white, box=(512,0))
   return res
