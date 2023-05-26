88949a9569d7eabedab76079ca8de9a0cd2d7bd8
duspic <duspic77@gmail.com>
2023-05-26 12:48:58 +0200

    mask shape fix

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
 def make_sheet(img: Image.Image, color: int=255) -> Image.Image:
-  res = Image.new('RGBA', (1024,512),(color,color,color))
+  res = Image.new('RGBA', (1024,512),color=color)
   w,h = img.size
   h_offset = int((512-h)/2)
   w_offset = int((256-w)/2)
@@ -11,8 +11,8 @@ def make_sheet(img: Image.Image, color: int=255) -> Image.Image:
   return res
 
 def make_mask() -> Image.Image:
-  res = Image.new('RGB', (1024,512), (0,0,0))
-  white = Image.new('RGB', (256,512), (255,255,255))
+  res = Image.new('RGBA', (1024,512), color=0)
+  white = Image.new('RGBA', (256,512), color=255)
 
   res.paste(white, box=(256,0))
   return res
