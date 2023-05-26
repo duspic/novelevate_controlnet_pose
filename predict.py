1602036c367efcd3655aa0c0024f34a35712e2ad
duspic <duspic77@gmail.com>
2023-05-26 12:40:42 +0200

    new version

--- predict.py
+++ predict.py
@@ -3,13 +3,12 @@
 
 from cog import BasePredictor, Input, Path
 from model import Model
-from annotator.util import resize_image, HWC3
-import cv2
 import os
 from PIL import Image
 from typing import List
 import numpy as np
 
+import utils
 
 class Predictor(BasePredictor):
     def setup(self):
@@ -18,8 +17,7 @@ class Predictor(BasePredictor):
     def predict(
         self,
         image: Path = Input(description="Input image"),
-        mask_image: Path = Input(description="B/W Mask image, white will be inpainted, black remains the same"),
-        controlnet_pose_image: Path = Input(description="Openpose image with pose(s) to generate"),
+        controlnet_pose_image: Path = Input(description="Openpose image with pose to generate"),
         prompt: str = Input(description="Prompt for the model"),
         a_prompt: str = Input(description="Additional text to be appended to prompt", default="""character turnaround sheet, multiple views of the same character,children's book, kid's book, illustration, beautiful, cute,character design, innocent"""),
         n_prompt: str = Input(description="Negative Prompt", default="""NSFW, sexy, seductive, miniskirt, adult,worst quality, low quality, jpeg artifacts, ugly, morbid, mutilated, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers,long neck"""),
@@ -43,16 +41,22 @@ class Predictor(BasePredictor):
         """Run a single prediction on the model"""
         
         input_img = Image.open(image)
+        input_img = utils.scale_for_sheet(input_img)
+        input_img = utils.make_sheet(input_img)
         input_img = np.array(input_img)
         
         pose_img = Image.open(controlnet_pose_image)
+        pose_img = utils.scale_for_sheet(pose_img)
+        pose_img = utils.make_sheet(pose_img, 0)
         pose_img = np.array(pose_img)  
         
-        mask_img = Image.open(mask_image)
+        mask_img = utils.make_mask()
         mask_img = np.array(mask_img)
 
         if not input_img.shape == pose_img.shape == mask_img.shape:
-            raise ValueError("The mask, pose and input image must have the same shape")
+            raise ValueError(f"""The mask, pose and input image must have the same shape
+                             input_img{input_img.shape}, pose_img{pose_img.shape}, mask_img{mask_img.shape}
+                             """)
         
         outputs = self.model.process_openpose(
             image=input_img,
@@ -73,5 +77,6 @@ class Predictor(BasePredictor):
         if not os.path.exists("tmp"):
             os.mkdir("tmp")
         
+        outputs = [utils.extract_char(output) for output in outputs]
         outputs = [output.save(f"tmp/output_{i}.png") for i, output in enumerate(outputs)]
         return [Path(f"./tmp/output_{i}.png") for i in range(len(outputs))]
---
+++ utils.py
@@ -0,0 +1,35 @@
+from PIL import Image
+
+def make_sheet(img: Image.Image, color: int=255) -> Image.Image:
+  res = Image.new('RGBA', (1024,512),(color,color,color))
+  w,h = img.size
+  h_offset = int((512-h)/2)
+  w_offset = int((256-w)/2)
+  for i in range(4):
+    res.paste(img, box=(i*256 + w_offset,h_offset))
+
+  return res
+
+def make_mask() -> Image.Image:
+  res = Image.new('RGB', (1024,512), (0,0,0))
+  white = Image.new('RGB', (256,512), (255,255,255))
+
+  res.paste(white, box=(256,0))
+  return res
+
+
+def scale_for_sheet(img: Image.Image) -> Image.Image:
+  maxwidth = 256
+  im_crop = img.crop(img.getbbox())
+  w,h = im_crop.size
+
+  if w > maxwidth:
+    ratio = maxwidth/w
+    h = int(ratio*h)
+    return im_crop.resize((256,h))
+
+  return im_crop
+
+
+def extract_char(res: Image.Image) -> Image.Image:
+  return res.crop((256,0,512,512))
