f16ac552e3ad44949d21dd0fcefbe2b2c4b549cd
duspic <duspic77@gmail.com>
2023-06-01 12:52:37 +0200

    fake sheet test

--- predict.py
+++ predict.py
@@ -42,7 +42,8 @@ class Predictor(BasePredictor):
         
         input_img = Image.open(image)
         input_img = utils.scale_for_sheet(input_img)
-        input_img = utils.make_sheet(input_img, skip_third=True)
+        #input_img = utils.make_sheet(input_img, skip_third=True)
+        input_img = Image.open("fake_sheet.png")
         input_img_np = np.array(input_img)
         
         pose_img = Image.open(controlnet_pose_image)
